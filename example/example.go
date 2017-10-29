a95b80c5974d541ecf0266d5b5dbc9961f9ada28
morikuni <taihei.with+git@gmail.com>
2017-10-30 00:07:55 +0900

    Add example

---
+++ example/example.go
@@ -0,0 +1,65 @@
+package example
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/morikuni/gen"
+)
+
+type User struct {
+	ID          int64
+	Name        string
+	ContactInfo ContactInfo
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type ContactInfo struct {
+	Email         string
+	EmailVerified bool
+}
+
+type Gen struct {
+	gen.Gen
+}
+
+func (g *Gen) User(key ...string) *User {
+	return GenUser(g.Rand(key))
+}
+
+func GenUser(r *rand.Rand) *User {
+	createdAt := *genTime(r).Gen()
+	return &User{
+		ID:          gen.Int(r).Gen64(),
+		Name:        gen.String(r).Len(8).Gen(),
+		ContactInfo: genContactInfo(r),
+		CreatedAt:   createdAt,
+		UpdatedAt:   *genTime(r).After(createdAt).Gen(),
+	}
+}
+
+func genContactInfo(r *rand.Rand) ContactInfo {
+	return ContactInfo{
+		Email:         genEmail(r),
+		EmailVerified: gen.Bool(r).Gen(),
+	}
+}
+
+func genEmail(r *rand.Rand) string {
+	return gen.String(r).Gen() + "@" + gen.String(r).Gen() + ".com"
+}
+
+func genTime(r *rand.Rand) *gen.TimeGen {
+	return gen.Time(r).
+		After(time.Date(2017, time.January, 1, 0, 0, 0, 0, time.Local)).
+		Before(time.Date(2017, time.February, 1, 0, 0, 0, 0, time.Local))
+}
+
+type KVS struct {
+	store map[int64]*User
+}
+
+func (kvs *KVS) Get(id int64) *User {
+	return kvs.store[id]
+}
---
+++ example/example_test.go
@@ -0,0 +1,32 @@
+package example
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStore_Get(t *testing.T) {
+	assert := assert.New(t)
+
+	gen := &Gen{}
+
+	kvs := setupKVS(gen)
+
+	assert.Equal(kvs.Get(gen.User("user1").ID), gen.User("user1"))
+	assert.Equal(kvs.Get(gen.User("user2").ID), gen.User("user2"))
+	assert.Equal(kvs.Get(gen.User("user3").ID), gen.User("user3"))
+}
+
+func setupKVS(gen *Gen) *KVS {
+	u1 := gen.User("user1")
+	u2 := gen.User("user2")
+	u3 := gen.User("user3")
+
+	m := map[int64]*User{}
+	m[u1.ID] = u1
+	m[u2.ID] = u2
+	m[u3.ID] = u3
+
+	return &KVS{m}
+}
